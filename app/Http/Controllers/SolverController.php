b8ff4ffc84cdb90ea77e1b30c4c35e37a9c6e979
Aung Naing Thu <aryalkrishna642@gmail.com>
2023-10-20 23:56:24 +0700

    Store the generated solutions in database

--- app/Http/Controllers/SolverController.php
+++ app/Http/Controllers/SolverController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Result;
+
 
 use IterTools\Multi;
 
@@ -20,43 +22,67 @@ class SolverController extends Controller
         $this->iterations = 0;
     }
 
-    public function solvePuzzle() {
+    public function solvePuzzle(Request $request) {
         $iterations = '';
 
-        $list = range(0,9);
+        if($request->action == 'action')
+        {
+            $list = range(0,9);
 
-        foreach ($this->pc_permute($list) as $key => $permutation) {
-            $permutations[] = implode(',', $permutation) . PHP_EOL; // Stored the permuted data in array.
-        }
-
-        $startTime = microtime(true);
-
-
-        foreach ($permutations as $perm) {
-
-            $letterToDigit = array_combine($this->letters, explode(',',$perm)); // Assign permuted numbers array to each Letter;
-
-            $hier = $this->replaceLettersWithDigits("HIER", $letterToDigit);
-            $gibt = $this->replaceLettersWithDigits("GIBT", $letterToDigit);
-            $es = $this->replaceLettersWithDigits("ES", $letterToDigit);
-            $neues =$this->replaceLettersWithDigits("NEUES", $letterToDigit);
-           
-            if ($hier + $gibt + $es == $neues) {
-                $solutions[] = [
-                    'HIER' => $hier,
-                    'GIBT' => $gibt,
-                    'ES' => $es,
-                    'NEUES' => $neues,
-                    'PERM' => $perm,
-                ];
+            foreach ($this->pc_permute($list) as $key => $permutation) {
+                $permutations[] = implode(',', $permutation) . PHP_EOL; // Stored the permuted data in array.
             }
 
-            $iterations++;
-        }
-       
-        $seconds = microtime(true) - $startTime;
+            $startTime = microtime(true);
 
-        return view('home')->with(['solutions' => $solutions, 'iterations' => $iterations, 'seconds' => $seconds]);
+
+            foreach ($permutations as $perm) {
+
+                //Assign permuted numbers array to each Letter
+                $letterToDigit = array_combine($this->letters, explode(',',$perm));
+                //print_r($lettrToDigit); 
+
+
+                $hier = $this->replaceLettersWithDigits("HIER", $letterToDigit);
+                $gibt = $this->replaceLettersWithDigits("GIBT", $letterToDigit);
+                $es = $this->replaceLettersWithDigits("ES", $letterToDigit); // Each word is replace with its own digit from $letterToDigit;
+                $neues =$this->replaceLettersWithDigits("NEUES", $letterToDigit);
+            
+                if ($hier + $gibt + $es == $neues) {
+                    $solutions[] = [
+                        'HIER' => $hier,
+                        'GIBT' => $gibt,
+                        'ES' => $es,
+                        'NEUES' => $neues,
+                        'PERM' => $perm,
+                    ];
+
+                }
+
+
+                $iterations++;
+            }
+        
+            $seconds = microtime(true) - $startTime;
+
+            // print_r($iterations);
+
+            // print_r("<br/>");
+
+            // die;
+            Result::create([
+                'letter_num' => json_encode($solutions),
+            ]);
+
+            return view('home')->with(['solutions' => $solutions, 'iterations' => $iterations, 'seconds' => $seconds]);
+
+        }else{
+
+            return view('home')->with(['solutions' => null, 'iterations' => 0, 'seconds' => 0]);
+
+        }
+
+        
     }
 
     function pc_permute(array $elements)
